34553ce4a1c1500c70d9c75d221a8029003bccb5
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-10-03 15:07:46 +0100

    imagen working code. Could't be me NGL

---
+++ assemberly/countro10.s
@@ -0,0 +1,15 @@
+TEN     DAT 10
+ONE     DAT 1
+CURENT  DAT 0
+        ADD ONE
+        STA CURENT
+        OUT CURENT
+        ADD ONE
+        STA CURENT
+        OUT CURENT
+        ADD ONE
+        STA CURENT
+        OUT CURENT
+        ADD ONE
+        STA CURENT
+        OUT CURENT
