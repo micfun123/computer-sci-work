a53a50334f16cb93390b9ee8d97baf204d170b03
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-10-06 10:45:05 +0100

    I am going mad from 10

---
+++ assemberly/countback.s
@@ -0,0 +1,19 @@
+        LDA back
+        out
+        LDA ONE
+        STA COUNT
+LOOPTOP LDA COUNT
+        ADD ONE
+        STA COUNT
+        SUB TEN
+        BRP ENDLOOP
+        LDA back
+        sub ONE
+        STA back
+        OUT
+        BRA LOOPTOP
+ENDLOOP HLT
+ONE     DAT 001
+TEN     DAT 010
+COUNT   DAT
+back    DAT 010
