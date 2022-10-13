877fa0e4baceb5b6828d7551b18eefb5201ecdbb
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-10-13 10:59:13 +0100

    do stuff

--- itemmerger.py
+++ itemmerger.py
@@ -1,10 +1,22 @@
+from collections import Counter
+
 def remove_dupe(inputelist):
     return list(dict.fromkeys(inputelist))
 
 list_one = ["cat","dog","panda","camel","pig"]
 list_two = ["cat","dog","panda","camel","pig","apple","cake"]
 
+#merget lists
 list_one = list_one + list_two
+
+#get ammounts
+amounts = Counter(list_one)
+
+#remove dups
 list_one = remove_dupe(list_one)
 
 print(list_one)
+print("\n")
+
+
+print(amounts)
