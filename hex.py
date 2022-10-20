576bb3a16c96ab33f163d5d5019eef53a7ebbac2
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-10-20 10:45:33 +0100

    to hex

---
+++ hex.py
@@ -0,0 +1,19 @@
+
+
+from secrets import token_hex
+
+
+raw_num = int(input("Enter a number: "))
+
+def tohex(num):
+    hex = []
+    while num > 0:
+        if num % 16 > 9:
+            hex.append(chr(num % 16 + 55))
+        else:
+            hex.append(num % 16)
+        num = num // 16
+    hex = hex[::-1]
+    return hex
+
+print(tohex(raw_num))
