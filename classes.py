2f8286b0592d1371f6ca040dd7300d82d02bd927
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-10-21 13:10:25 +0100

    pet

---
+++ classes.py
@@ -0,0 +1,20 @@
+
+
+class person:
+    def __init__(self, name: str , age: int , money :float):
+        self.name = name
+        self.age = age
+        self.money = money
+        print("class has been initilised")
+
+    def description(self):
+        return f"{self.name} is {self.age} years old"
+    
+
+michael = person("michael", 16, 2.50)
+
+
+print(michael)
+
+print(michael.description())
+
