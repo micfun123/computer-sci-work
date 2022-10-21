e3d5b16689f03c7dbd78b7232cfb0b12c3de7a8d
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-10-21 13:23:40 +0100

    cute code ngl

--- digitalpet.py
+++ digitalpet.py
@@ -1,7 +1,8 @@
+from random import choices
 from ssl import Options
 
 
-pet = ""
+
 
 class pet:
     def __init__(self, name: str, type: str, age: int):
@@ -15,14 +16,11 @@ class pet:
     def age(self) -> int:
         self.age = self.age + 1
         return self.age
-print("enter a option")
-options = ["start game","exit",'make new pet']
 
-for i in options:
-    print(i)
-    
-user_option = input()
 
-while user_option not in options:
-    user_option = input("enter a valid option \n")
+pet.name = input("Enter a pets name")
+pet.type = input("Enter a pets type")
+pet.age = 0
+
+
 
