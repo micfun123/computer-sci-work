ca2abf4d6b1f4553b8eb8a3bf76b249e03edc49d
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-10-26 19:56:00 +0100

    hmm

--- fibbing.c
+++ fibbing.c
@@ -9,15 +9,16 @@ int main(){
 
     printf("What should be the starting number: ");
     scanf("%d",&b);
-    printf("How far should we go up: ");
+    printf("How many places should we go up: ");
     scanf("%d",&amount);
 
     for (int i = 0; i < amount; i++)
     {
         z = a + b;
+        total = total + z;
         printf("%d ",z);
         a = b;
         b = z;
     }
-    
+    printf("%d ",total);
 }
