d0396db9a68a916cfe8a8ebf29a50fa29453d606
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-10-26 20:02:19 +0100

    worked sure

--- fibbing.c
+++ fibbing.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int main(){
+void main(){
     int a = 0;
-    int b;
-    int z;
-    int total;
-    int amount;
+    int b = 0;
+    int z = 0;
+    int total = 0;
+    int amount = 0;
 
     printf("What should be the starting number: ");
     scanf("%d",&b);
@@ -20,5 +20,5 @@ int main(){
         a = b;
         b = z;
     }
-    printf("%d ",total);
+    printf("\n your total is %d ",total);
 }
