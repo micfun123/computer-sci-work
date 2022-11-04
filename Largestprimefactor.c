d2d054474919f71236f127ec212fc8ee62a32be8
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-11-04 20:22:33 +0000

    600851475143

--- Largestprimefactor.c
+++ Largestprimefactor.c
@@ -3,8 +3,10 @@
 #include <stdio.h>
 
 void main(){
-    long long int target = 600851475143;
+    long long int target;
     long long int i = 2;
+    printf("enter the target numbers: ");
+    scanf("%d", &target);
     while(i < target){
         if(target % i == 0){
             target = target / i;
