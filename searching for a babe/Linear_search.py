f5a740a23c90b14b1474827ec91cd99c99e43ed5
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-11-11 12:59:01 +0000

    for and while

---
+++ searching for a babe/Linear_search.py
@@ -0,0 +1,15 @@
+
+
+def Linear_search_for(arr, key):
+    for i in range(len(arr)):
+        if arr[i] == key:
+            return i
+    return -1
+
+def Linear_search_while(arr, key):
+    i = 0
+    while i < len(arr):
+        if arr[i] == key:
+            return i
+        i += 1
+    return -1
