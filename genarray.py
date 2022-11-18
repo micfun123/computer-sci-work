f6bf7fc24fd2fa95539e76be84104a13ff810131
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-11-18 13:09:31 +0000

    i needed a array dont ask

--- genarray.py
+++ genarray.py
@@ -2,6 +2,8 @@ import random
 num = []
 for i in range(1000):
     add = random.randint(100,999)
-    num.append(str(add))
+    num.append(add)
 
-print(num)
+num.sort()
+strnum = [str(i) for i in num]
+print(strnum)
