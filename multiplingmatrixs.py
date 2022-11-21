272de92187459cc4df6138dca10fea11117ed8bf
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-11-21 12:32:37 +0000

    um

---
+++ multiplingmatrixs.py
@@ -0,0 +1,1 @@
+return Matrix(*[[sum([val*column[i] for val,column in zip(row, other.rows)]) for i,_ in enumerate(other.rows[0])] for row in self.rows])
--- sorting my life (sort algarithoms)/bongosort.py
+++ sorting my life (sort algarithoms)/bongosort.py
@@ -21,18 +21,18 @@ def bogoSort(a):
 
 # To check if array is sorted or not
 def is_sorted(a):
-	n = len(a)
-	for i in range(0, n-1):
-		if (a[i] > a[i+1] ):
-			return False
-	return True
+    n = len(a)
+    for i in range (0,n-1):
+        if (a[i] > a[i+1] ):
+            return False
+    return True
 
 # To generate permutation of the array
 def shuffle(a):
-	n = len(a)
-	for i in range (0,n):
-		r = random.randint(0,n-1)
-		a[i], a[r] = a[r], a[i]
+    n = len(a)
+    for i in range (0,n):
+        r = random.randint(0,n-1)
+        a[i], a[r] = a[r], a[i]
 
 # Driver code to test above
 
