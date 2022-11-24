a5c6fa28d4d1ca07b8ef9fef2812df6d33cf382d
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-11-24 10:55:44 +0000

    code goes brrrr

--- caretaker_game/main.py
+++ caretaker_game/main.py
@@ -2,11 +2,38 @@
 
 class Critter(object):
     """Makes a virtual pet"""
+
+    total = 0
+
+    @staticmethod
+    def status():
+        print("\nTotal critters =", Critter.total)
+        
+    @property
+    def mood(self):
+        unhappiness = self.hunger + self.boredom
+        if unhappiness < 5:
+            m = "happy"
+        elif 5 <= unhappiness <= 10:
+            m = "okay"
+        elif 11 <= unhappiness <= 15:
+            m = "frustrated"
+        else:
+            m = "mad"
+        return m
+
     def __init__(self, name):
         self.name = name
         self.hunger = 0
         self.boredom = 0
         self.mood = "happy"
+        Critter.total += 1
+
+    def __pass_time(self):
+        self.hunger += 1
+        self.boredom += 1
+        self.update_mood()
+
 
     def __str__(self) -> str:
         rep = "Critter object"
@@ -18,3 +45,5 @@ class Critter(object):
 
 crit = Critter("Michael")
 crit.talk()
+
+Critter.status()
