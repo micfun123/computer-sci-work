4bf317fe5f78043685b92eeb856956acca68d1d6
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-12-08 10:00:58 +0000

    words

---
+++ words_amount.py
@@ -0,0 +1,9 @@
+#how many repeated workds
+string = input("Enter a string: ")
+words = string.split()
+words_amount = len(words)
+print("The amount of words is: ", words_amount)
+words = set(words)
+words_amount = len(words)
+print("The amount of unique words is: ", words_amount)
+
