660cbf4293165a1ca9c8d2821ebc00fee4305dc4
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-12-09 13:05:04 +0000

    extract_via_dictionary

---
+++ dictionary_compression.py
@@ -0,0 +1,32 @@
+import string
+
+userinpute = "A cat sat on the bed, a cat sat on the chair, The dog sat under the bed, The dog sat under the chair."
+
+
+def compress_via_dictionary(str) -> str:
+    istringids = []
+    Words = []
+    str = str.lower()
+    for i in string.punctuation:
+        str = str.replace(i, '')
+    for i in str.split():
+        if i in Words:
+            pass
+        else:
+            Words.append(i)
+    for j in str.split():
+        istringids.append(Words.index(j))
+    return istringids , Words
+    
+
+def extract_via_dictionary(idarray,wordarray) -> dict:
+    sentance = []
+    print(idarray)
+    for i in idarray:
+        i = int(i)
+        sentance.append(wordarray[i])
+
+    return " ".join(sentance)
+        #get last word id and add a new id
+ids,words = compress_via_dictionary(userinpute)
+print(extract_via_dictionary(ids,words))
