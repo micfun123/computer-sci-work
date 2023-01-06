03f90346a3746890f548cca84911f409722897a6
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-01-06 13:05:00 +0000

    tiny exchange rate app

--- tkinter/exchange.py
+++ tkinter/exchange.py
@@ -1,23 +1,40 @@
 from tkinter import *
 
+current_exchange_rate= {
+    'EUR' : 1.17,
+    'USD' : 1.31,
+    'JPY' : 132.53,
+    'AED' : 4.75
+}
+
 class Exchange(Frame):
     def __init__(self, parent=None):
         Frame.__init__(self, parent)
         self.pack()
         self.makeWidgets()
 
+        # Change the label text
+    def show_drop(self):
+        self.result.config(text=self.to_currency.get())
+               
+
+
     def makeWidgets(self):
         Label(self, text="Enter amount in GBP:").pack(side=LEFT)
         self.gbp = Entry(self)
+        self.to_currency = Entry(self)
         self.gbp.pack(side=LEFT)
+        #show dropdown
+        self.to_currency.pack(side=LEFT)
+        self.to_currency.bind("<<ComboboxSelected>>", self.show_drop)
         Button(self, text="Convert", command=self.onPress).pack(side=LEFT)
         self.result = Label(self, text="Result")
         self.result.pack(side=LEFT)
 
     def onPress(self):
         gbp = float(self.gbp.get())
-        eur = gbp * 1.17
-        self.result.config(text="%.2f" % eur + " EUR")
+        result = gbp * current_exchange_rate[self.to_currency.get()] 
+        self.result.config(text="%.2f" % result + " "+  self.to_currency.get())
 
 
 if __name__ == '__main__': Exchange().mainloop()
