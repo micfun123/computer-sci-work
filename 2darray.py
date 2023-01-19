7bf81a0120def20076cf9ec7db344c23c1cc069e
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-01-19 10:33:00 +0000

    2d arrays

---
+++ 2darray.py
@@ -0,0 +1,17 @@
+staff = ["anna", "bob", "charlie"]
+sales = [[100,110,120,110], [200,210,220,210], [300,310,320,310]]
+
+total = 0
+
+print("Lets see how much each staff member sold")
+for i in range(len(staff)):
+    print(staff[i], "sold", sum(sales[i]), "in total") 
+    for j in range(len(sales[i])):
+        print("On Quarter", j+1, "they sold", sales[i][j])
+    print("\n")
+    total += sum(sales[i])
+print("Total sales:", total)
+
+        
+        
+
