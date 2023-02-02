ca156e088664e5d683f5c78adee44b426a22faae
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-02-02 10:43:20 +0000

    transversed graph

---
+++ traversing_graphs.py
@@ -0,0 +1,27 @@
+
+graph = [["A", "B",15], ["A","D",15], ["A","E",5], ["B","C",10], ["B","E",5], ["C","D",15], ["C","F",15],["C","E",5], ["D","F",5], ["E","F",25]]
+
+#CREATE stack
+#MARK startingNode as VISITED AND PUSH currentNode onto stack
+#WHILE stack NOT EMPTY
+#	currentNode = topOfStack
+#	WHILE currentNode has (unvisited) neighbour
+#		MARK neighbourNode as VISITED AND PUSH onto stack
+#		currentNode = topOfStack
+#	POP topOfStack
+#
+
+def traverse_graph(graph, start_node):
+    stack = []
+    visited = []
+    stack.append(start_node)
+    while len(stack) > 0:
+        current_node = stack.pop()
+        if current_node not in visited:
+            visited.append(current_node)
+            for node in graph:
+                if node[0] == current_node and node[1] not in visited:
+                    stack.append(node[1])
+    return visited
+
+print(traverse_graph(graph, "A"))
