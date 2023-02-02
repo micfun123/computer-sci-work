d087b5b041e23248a9745281db217997e597a9e4
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-02-02 10:52:32 +0000

    queue

--- traversing_graphs.py
+++ traversing_graphs.py
@@ -11,7 +11,7 @@ graph = [["A", "B",15], ["A","D",15], ["A","E",5], ["B","C",10], ["B","E",5], ["
 #	POP topOfStack
 #
 
-def traverse_graph(graph, start_node):
+def stack_traverse_graph(graph, start_node):
     stack = []
     visited = []
     stack.append(start_node)
@@ -24,4 +24,29 @@ def traverse_graph(graph, start_node):
                     stack.append(node[1])
     return visited
 
-print(traverse_graph(graph, "A"))
+
+print(stack_traverse_graph(graph, "A"))
+
+
+#CREATE queue
+#MARK startingNode as VISITED AND ENQUEUE startingNode into queue
+#WHILE queue NOT EMPTY
+#	DEQUEUE startOfQueue
+#	MARK and ENQUEUE (unvisited) neighbour(s) of dequeuedNode
+
+
+def queue_traverse_graph(graph, start_node):
+    stack = []
+    visited = []
+    stack.append(start_node)
+    while len(stack) > 0:
+        current_node = stack.pop(0)
+        if current_node not in visited:
+            visited.append(current_node)
+            for node in graph:
+                if node[0] == current_node and node[1] not in visited:
+                    stack.append(node[1])
+    return visited
+
+
+print(queue_traverse_graph(graph, "A"))
