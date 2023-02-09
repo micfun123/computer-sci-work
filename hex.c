2d768ac95d408148d443e5767cace3b8ed8080bf
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-02-09 13:04:54 +0000

    hex

---
+++ hex.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+void to_hex(int num){
+    int hex[100];
+    int i = 0;
+    while (num != 0)
+    {
+        hex[i] = num % 16;
+        num = num / 16;
+        i++;
+    }
+    for (int j = i - 1; j >= 0; j--)
+    {
+        if (hex[j] > 9)
+        {
+            printf("%c", hex[j] + 55);
+        }else
+        {
+            printf("%d", hex[j]);
+        }
+    }
+    
+}
+
+
+void main(){
+    int num = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    to_hex(num);
+}
