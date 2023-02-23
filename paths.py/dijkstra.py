4dd1804507e92755de722c75eb89da695600c61d
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-02-23 09:55:16 +0000

    formater

--- paths.py/dijkstra.py
+++ paths.py/dijkstra.py
@@ -1,16 +1,14 @@
-
-
-#graph data structure
+# graph data structure
 graph = {
-    'A': {'B': 5, 'C': 1},
-    'B': {'A': 5, 'C': 2, 'D': 1},
-    'C': {'A': 1, 'B': 2, 'D': 4, 'E': 8},
-    'D': {'B': 1, 'C': 4, 'E': 3, 'F': 6},
-    'E': {'C': 8, 'D': 3},
-    'F': {'D': 6}
+    "A": {"B": 5, "C": 1},
+    "B": {"A": 5, "C": 2, "D": 1},
+    "C": {"A": 1, "B": 2, "D": 4, "E": 8},
+    "D": {"B": 1, "C": 4, "E": 3, "F": 6},
+    "E": {"C": 8, "D": 3},
+    "F": {"D": 6},
 }
 
-#dijkstra's algorithm
+# dijkstra's algorithm
 def dijkstra(graph, start, goal):
     shortest_distance = {}
     infinite = 9999999
@@ -41,16 +39,12 @@ def dijkstra(graph, start, goal):
             path.insert(0, current_node)
             current_node = privious_node[current_node]
         except KeyError:
-            print('Path not reachable')
+            print("Path not reachable")
             break
     path.insert(0, start)
     if shortest_distance[goal] != infinite:
-        print('Shortest distance is ' + str(shortest_distance[goal]))
-        print('And the path is ' + str(path))
+        print("Shortest distance is " + str(shortest_distance[goal]))
+        print("And the path is " + str(path))
 
 
-
-
-
-
-dijkstra(graph, 'A', 'F')
+dijkstra(graph, "A", "F")
