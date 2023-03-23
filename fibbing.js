25a57cc9722eef6b053a216e09109a612d81f8f1
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-03-23 10:28:25 +0000

    fibbing

--- fibbing.js
+++ fibbing.js
@@ -1,19 +1,31 @@
 
 var a = 0;
 var b = 1;
-var end = 10;
-var c = 0;
-var total = 0;
+//set the end of the sequence via user input
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+  
+readline.question(`What's the amount of times to go throught?`, end => {
+    //turn end in to a int
+    end = parseInt(end);
+    
+    var c = 0;
+    var total = 0;
 
-list = [];
-for (var i = 0; i < end; i++) {
-    c = a + b;
-    a = b;
-    b = c;
-    list.push(c);
-    total += c;
+    list = [];
+    for (var i = 0; i < end; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+        list.push(c);
+        total += c;
 
-}
+    }
 
-console.log(list);
-console.log(total);
+    console.log(list);
+    console.log(total);
+
+    readline.close();
+});
