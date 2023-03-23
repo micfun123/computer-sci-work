db663f993b0b7b2097a6c1e48288d2f2afe94fdd
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-03-23 10:21:31 +0000

    js fibbing

--- fibbing.js
+++ fibbing.js
@@ -1,12 +1,12 @@
 
-// do the fibinarchi thing
 var a = 0;
 var b = 1;
+var end = 10;
 var c = 0;
 var total = 0;
 
 list = [];
-for (var i = 0; i < 10; i++) {
+for (var i = 0; i < end; i++) {
     c = a + b;
     a = b;
     b = c;
