85b71a2afa0e8afea39da24b913c472685d6f7cf
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-03-27 15:01:05 +0100

    email

---
+++ validate_email.py
@@ -0,0 +1,16 @@
+import re
+
+banned_char = ["!", "#", "$", "%", "&", "'", "*", "+", "-", "/", "=", "?", "^", "_", "`", "{", "|", "}", "~", " "]
+
+def email_check(email):
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        return False
+    if len(email) > 256:
+        return False
+    for char in banned_char:
+        if char in email:
+            return False
+    return True
+    
+
+print(email_check(""))
