64224afba72c6ea4b3e87497d67d335692625e78
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-03-31 13:13:07 +0100

    mpn

--- mrhalls_maths/mpn.py
+++ mrhalls_maths/mpn.py
@@ -1,17 +1,26 @@
 # p^n + 3600 = m^2
-tosolve = 100
+
+tosolve = 200
 
 solution = []
 
+#find p n and m. P is a prime
+
+def isprime(n):
+    if n == 1:
+        return False
+    if n == 2:
+        return True
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
 
 for p in range(1, tosolve):
-    for n in range(p+1, tosolve):
-        for m in range(n+1, tosolve):
-            if p**n + 3600 == m**2:
-                solution.append(f"{p} , {n}, {m}")
-            print(p,m,n)
-    print("Row done")
+    if isprime(p):
+        for n in range(1, tosolve):
+            for m in range(1, tosolve):
+                if p ** n + 3600 == m ** 2:
+                    solution.append((p, n, m))
 
 print(solution)
-
-
