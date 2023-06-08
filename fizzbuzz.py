fd7ede56389e6d2502b9810b3fcd7ead8591b71c
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-06-08 21:51:09 +0100

    Fizz buxx C and py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,18 +3,17 @@
 
 def fizzBuzz(n: int):
     output = []
-    for i in range(1, n+1):
-            string = ""
-            if i % 3 == 0:
-                string = string + "Fizz" 
-            if i % 5 == 0:
-                string = string + "Buzz"
-            if string == "":
-                string = i
-
-            output.append(string)
-            string = ""
+    string = ""
+    if n % 3 == 0:
+        string = string + "Fizz" 
+    if n % 5 == 0:
+        string = string + "Buzz"
+    if string == "":
+        string = n
+    output.append(string)
+    string = ""
     return output
 
 
-print(fizzBuzz(15))
+for i in range(1, 101):
+    print(fizzBuzz(i))
