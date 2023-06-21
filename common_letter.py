22cca7d92a07093ab75c052371ef1db08dce4ca9
Michael Parker <michaelrbparker@gmail.com>
2023-06-21 21:19:57 +0100

    common letter

--- common_letter.py
+++ common_letter.py
@@ -1,16 +1,28 @@
 
-teststr = "eadawdawdawd"
+teststr = "eaeeeeeedawdawdawd"
 
 def common_letter(teststr):
     teststr = teststr.lower()
-    letters = {}
+
+
+    letters = {} #makes a dictionary of letters and their counts
+
+
     for letter in teststr:
         if letter in letters:
-            letters[letter] += 1
+            letters[letter] += 1 #adds to count if letter already in dictionary
         else:
-            letters[letter] = 1
+            letters[letter] = 1 #adds letter to dictionary if not already there
 
-    print(letters)
+
+    maxletter = "" #placeholder for most common letter name
+    maxcount = 0 #placeholder for most common letter count
+    for letter in letters:
+        if letters[letter] > maxcount: #checks if current letter is more common than previous most common letter
+            maxletter = letter
+            maxcount = letters[letter] #sets new most common letter and count
+    return maxletter
+
 
 
 print(common_letter(teststr))
