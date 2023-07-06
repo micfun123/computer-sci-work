912095a03308d1134662203ba33b6980a21c5083
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-07-06 10:16:53 +0100

    20219 paper hasher

---
+++ 2019paperhasher.py
@@ -0,0 +1,15 @@
+
+sitename = "www.orc.org"
+
+
+def hasher(site):
+    site = site.split(".")[1]
+    site = site.split(".")[0]
+    site = site.upper()
+    site = [ord(c) for c in site]
+    total = 0
+    for i in site:
+        total += i
+    return total
+
+print(hasher(sitename))
