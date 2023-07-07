2e9e0c05d6b7554f7c04144b6596a3596490d810
Michael Parker <michaelrbparker@gmail.com>
2023-07-07 19:15:29 +0100

    Linear_search

--- searching for a life (algarithoms)/binary_search.c
+++ searching for a life (algarithoms)/binary_search.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
 
-int to_search[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int to_search[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 55};
 
 
 int binary_search(int to_search[], int size, int value)
