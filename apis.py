b45243ae453478dce0aa3fd02e1d8484a9a6c519
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-07-24 08:49:11 +0100

    API demo

---
+++ apis.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+url = "https://data.binance.com/api/v3/ticker/24hr" #url
+request = requests.get(url) #request data
+data = json.loads(request.text) #load data to json
+print(data) #print data
+#find price of bitcoin
+for i in data:
+    if i['symbol'] == 'BTCUSDT':
+        print(i['lastPrice'])
+        
---
+++ dealwithwords.py
@@ -0,0 +1,7 @@
+#open words.txt
+
+file = open('words.txt', 'r') #r is for read, w is for write, a is for append
+print(file.read())
+print(file.readline())
+#when finnished with file, close it 
+file.close()
