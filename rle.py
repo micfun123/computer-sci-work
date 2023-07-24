5cbf7b590329194a37c43742c406edacfb600345
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-07-24 09:03:17 +0100

    fixed encoder

--- rle.py
+++ rle.py
@@ -6,25 +6,20 @@ def rle_encoder(string):
     """
     result = ""
     count = 1
-    for i in range(len(string)-1):
-        if string[i] == string[i+1]:
+    for i in range(0,len(string)):
+        if i == 0:
+            result += string[i]
+        elif string[i] == string[i-1]:
             count += 1
         else:
-            result += string[i] + str(count)
+            result += str(count) + string[i]
             count = 1
-    result += string[i] + str(count)
+    result += str(count)
     return result
 
-def rle_decoder(string):
-    """
-    rle(c3a2b1) -> cccaab
-    """
-    result = ""
-    for i in range(0,len(string),2):
-        result += string[i] * int(string[i+1])
-    return result
+
+
 
 
 print(rle_encoder(string))
-print(rle_decoder(rle_encoder(string)))
 
