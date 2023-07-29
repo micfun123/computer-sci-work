f0370c1c3514ec312ddb87b38b3d79e1a0c60b96
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-07-29 20:38:40 +0100

    string parse calc

--- string_calc_parser.py
+++ string_calc_parser.py
@@ -1,30 +1,75 @@
 
 def string_calc_parser(equation):
     #turns "1+2" into 3 and "1+2+3" into 6 and "12 + 3" into 15
-    numbers = []
-    operators = []
-    current_number = ""
+   #if not a digit or operator, remove it
+    
+    allowed_characters = "0123456789+-*/"
+    for char in equation:
+        if char not in allowed_characters:
+            equation = equation.replace(char, "")
+    if equation == "":
+        return 0
+    if equation == None:
+        return 0
+    tosolve = []
     for char in equation:
         if char.isdigit():
-            current_number += char
+            tosolve.append(int(char))
         else:
-            numbers.append(int(current_number))
+            tosolve.append(char)
+    #join the digits together
+    current_number = ""
+    new_list = []
+    for item in tosolve:
+        if type(item) == int:
+            current_number += str(item)
+        else:
+            new_list.append(int(current_number))
+            new_list.append(item)
             current_number = ""
-            operators.append(char)
-    numbers.append(int(current_number))
-    result = numbers[0]
-    for i in range(len(operators)):
-        if operators[i] == "+":
-            result += numbers[i+1]
-        elif operators[i] == "-":
-            result -= numbers[i+1]
-        elif operators[i] == "*":
-            result *= numbers[i+1]
-        elif operators[i] == "/":
-            result /= numbers[i+1]
+    new_list.append(int(current_number))
+    #solve the equation using order of operations
+    while len(new_list) > 1:
+        for i in range(len(new_list)):
+            if new_list[i] == "*":
+                new_list[i] = new_list[i+1] *  new_list[i-1]
+                new_list.pop(i-1)
+                new_list.pop(i)
+                break
+            elif new_list[i] == "/":
+                new_list[i] = new_list[i-1] / new_list[i+1]
+                new_list.pop(i-1)
+                new_list.pop(i)
+                break
+            elif new_list[i] == "+":
+                new_list[i] = new_list[i-1] + new_list[i+1]
+                new_list.pop(i-1)
+                new_list.pop(i)
+                break
+            elif new_list[i] == "-":
+                new_list[i] = new_list[i-1] - new_list[i+1]
+                new_list.pop(i-1)
+                new_list.pop(i)
+                break
 
-    return result
+    return new_list[0]
 
-print(string_calc_parser("1+2"))
-print(string_calc_parser("12+3"))
-        
+           
+    
+    
+
+def test(enter,expected):
+    out = string_calc_parser(enter)
+    if out != expected: 
+        print( f"tested for {enter} and failed. \n We got {out} \n We expected {expected}")
+    else:
+        print(True)
+
+test("1+2",3)
+test("1+22",23)
+test("1+2 2",23)
+test("1",1)
+test("2 *2",4)
+test("   2 * 2-1",3)
+test("  ",0)
+
