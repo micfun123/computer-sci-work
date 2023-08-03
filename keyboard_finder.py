5f86238b1ac598d8a30e530673f8b50a553d9b0b
Michael Parker <michaelrbparker@protonmail.com>
2023-08-03 08:41:44 +0100

    keyboard key locatior

---
+++ keyboard_finder.py
@@ -0,0 +1,12 @@
+configuration_of_keyboard = [["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+                             ["a", "s", "d", "f", "g", "h", "j", "k", "l", ";"],
+                             ["z", "x", "c", "v", "b", "n", "m", ",", ".", "/"]]
+
+
+def find_quards(key):
+    for i in range(len(configuration_of_keyboard)):
+        for j in range(len(configuration_of_keyboard[i])):
+            if configuration_of_keyboard[i][j] == key:
+                return i, j
+            
+print(find_quards("c"))
