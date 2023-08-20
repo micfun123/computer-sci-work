94a4100c9cbbb05261f21df300e63d4bd6d52d4b
Michael Parker <michaelrbparker@protonmail.com>
2023-08-20 08:31:30 +0100

    mario

---
+++ mario.py
@@ -0,0 +1,13 @@
+
+def pyramid(height):
+    for i in range(1, height + 1):
+        for space in range(height - i):
+            print(" ", end="")
+        for hash in range(i):
+            print("#", end="")
+        print(" ", end="")
+        for hash in range(i):
+            print("#", end="")
+        print()
+
+pyramid(4)
