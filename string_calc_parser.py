a40d54823f60c4193ec338323408080f9b9c1c8b
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-08-31 20:07:30 +0100

    simple

--- string_calc_parser.py
+++ string_calc_parser.py
@@ -41,6 +41,62 @@ def string_calc_parser(equation):
     
     return result
 
+def string_calc_parser_simple(equation):
+    #turns "1+2" into 3 and "1+2+3" into 6 and "12 + 3" into 15
+   #if not a digit or operator, remove it
+    
+    allowed_characters = "0123456789+-*/"
+    for char in equation:
+        if char not in allowed_characters:
+            equation = equation.replace(char, "")
+    if equation == "":
+        return 0
+    if equation == None:
+        return 0
+    tosolve = []
+    for char in equation:
+        if char.isdigit():
+            tosolve.append(int(char))
+        else:
+            tosolve.append(char)
+    #join the digits together
+    current_number = ""
+    new_list = []
+    for item in tosolve:
+        if type(item) == int:
+            current_number += str(item)
+        else:
+            new_list.append(int(current_number))
+            new_list.append(item)
+            current_number = ""
+    new_list.append(int(current_number))
+    #solve the equation using order of operations
+    while len(new_list) > 1:
+        for i in range(len(new_list)):
+            if new_list[i] == "*":
+                new_list[i] = new_list[i+1] *  new_list[i-1]
+                new_list.pop(i-1)
+                new_list.pop(i)
+                break
+            elif new_list[i] == "/":
+                new_list[i] = new_list[i-1] / new_list[i+1]
+                new_list.pop(i-1)
+                new_list.pop(i)
+                break
+            elif new_list[i] == "+":
+                new_list[i] = new_list[i-1] + new_list[i+1]
+                new_list.pop(i-1)
+                new_list.pop(i)
+                break
+            elif new_list[i] == "-":
+                new_list[i] = new_list[i-1] - new_list[i+1]
+                new_list.pop(i-1)
+                new_list.pop(i)
+                break
+
+    return new_list[0]
+
+
 # Test cases
 print(string_calc_parser("1+2"))    # Output: 3
 print(string_calc_parser("1+2+3"))  # Output: 6
