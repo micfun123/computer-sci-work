56a69f044cd910f90b1654da08ab5021e6f9c66e
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-09-18 12:58:57 +0100

    passords

--- passwords.py
+++ passwords.py
@@ -1,23 +1,28 @@
 import random
 
-alphabet = "abcdefghijklmnopqrstuvwxyz!@#()+_"
-pw_length = 8
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+pw_length = 12
 mypw = ""
 
-for i in range(pw_length):
-    next_index = random.randrange(len(alphabet))
-    mypw = mypw + alphabet[next_index]
+def pw_gen():
+    for i in range(pw_length):
+        next_index = random.randrange(len(alphabet))
+        mypw = mypw + alphabet[next_index]
 
 
-for i in range(random.randrange(1,3)):
-    replace_index = random.randrange(len(mypw)//2)
-    mypw = mypw[0:replace_index] + str(random.randrange(10)) + mypw[replace_index+1:]
+    for i in range(random.randrange(1,3)):
+        replace_index = random.randrange(len(mypw)//2)
+        mypw = mypw[0:replace_index] + str(random.randrange(10)) + mypw[replace_index+1:]
 
 
-for i in range(random.randrange(1,3)):
-    replace_index = random.randrange(len(mypw)//2,len(mypw))
-    mypw = mypw[0:replace_index] + mypw[replace_index].upper() + mypw[replace_index+1:]
+    for i in range(random.randrange(1,3)):
+        replace_index = random.randrange(len(mypw)//2,len(mypw))
+        mypw = mypw[0:replace_index] + mypw[replace_index].upper() + mypw[replace_index+1:]
+
+    punctuation = "!@#$%^&*()_+~`|}{[]\:;?><,./'"
+    for i in range(random.randrange(1,3)):
+        replace_index = random.randrange(len(mypw)//2,len(mypw))
+        mypw = mypw[0:replace_index] + random.choice(punctuation) + mypw[replace_index+1:]
 
 print(mypw)
 
-k=input("press close to exit") 
