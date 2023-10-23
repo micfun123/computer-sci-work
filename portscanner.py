d74911ca30bd182304f061034305c4353912253d
Michael Parker <michaelrbparker@protonmail.com>
2023-10-23 16:12:00 +0100

    waning

--- portscanner.py
+++ portscanner.py
@@ -22,6 +22,7 @@ if options == "c":
 elif options == "o":
     ports = [20,21,22,23,25,53,67,68,69,80,110,119,123,137,138,139,143,161,162,389,443,445,465,514,587,636,993,995,1433,1521,1723,3306,3389,5432,5900,8080,10000]
 elif options == "a":
+    print(f"Waring, this will take a long time to scan all ports on {target}, Scanning all ports is not recommended")
     ports = [x for x in range(1,65536)]
 else:
     print("invalid option, using common ports")
