ac1334a4f4a9cd111cfb4f05396fc75664e2b6de
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-11-04 20:55:03 +0000

    type racer

---
+++ typeracer.py
@@ -0,0 +1,27 @@
+import time
+
+def type_racer(source_text, time_limit=60):
+    print("Welcome to Type Racer!")
+    print("Type the following text as fast as you can:")
+    print(source_text)
+    input("Press Enter to start...")
+    
+    start_time = time.time()
+    player_input = input("Type here: ")
+    end_time = time.time()
+    
+    elapsed_time = end_time - start_time
+    total_characters = len(source_text)
+    correct_characters = sum(a == b for a, b in zip(source_text, player_input))
+    
+    typing_speed = (correct_characters / 5) / (elapsed_time / 60)  # Assuming an average word length of 5 characters
+    accuracy = (correct_characters / total_characters) * 100
+    
+    print(f"\nTime taken: {elapsed_time:.2f} seconds")
+    print(f"Typing speed: {typing_speed:.2f} words per minute")
+    print(f"Accuracy: {accuracy:.2f}%")
+    print(f"Total errors: {total_characters - correct_characters}")
+
+if __name__ == "__main__":
+    source_text = "This is a simple Type Racer game. Test your typing skills!"
+    type_racer(source_text)
