f72550f15092f3105ccb31a33067afcc0805f6bc
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-11-06 13:19:35 +0000

    formating

--- binary_tree.py
+++ binary_tree.py
@@ -1,11 +1,10 @@
-
 class BinaryTree:
-    def __init__(self,value) -> None:
+    def __init__(self, value) -> None:
         self.left = None
         self.right = None
         self.value = value
 
-    def insert(self,value):
+    def insert(self, value):
         if value < self.value:
             if self.left is None:
                 self.left = BinaryTree(value)
@@ -26,18 +25,18 @@ class BinaryTree:
         if self.right:
             self.right.print_tree()
 
-    def find(self,value):
+    def find(self, value):
         if value < self.value:
             if self.left is None:
-                return str(value)+" Not Found"
+                return str(value) + " Not Found"
             return self.left.find(value)
         elif value > self.value:
             if self.right is None:
-                return str(value)+" Not Found"
+                return str(value) + " Not Found"
             return self.right.find(value)
         else:
-            return str(self.value)+" Found"
-        
+            return str(self.value) + " Found"
+
 
 tree = BinaryTree(12)
 tree.insert(6)
