7d1925f518e1562897faf26ef6626b97013752a2
Michael Parker <michaelrbparker@protonmail.com>
2023-11-24 09:31:53 +0000

    spinner 2

--- spinner.py
+++ spinner.py
@@ -1,11 +1,16 @@
 import time
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k','l','m','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
 def spinner(word):
     for i in range(len(word)):
-        for j in range(len(alphabet)):
-            print(f"\r{word[:i]}{alphabet[j]}", end="") #print the word up to the current letter, then the current letter
+        if word[i] in alphabet:
+            pos = alphabet.index(word[i])
+            for j in range(pos + 1):
+                print(f"\r{word[:i]}{alphabet[j % len(alphabet)]}", end="")
+                time.sleep(0.05)
+        else:
+            print(f"\r{word[:i]}{word[i]}", end="")
             time.sleep(0.05)
     print("\r", end="")
     print(word)
@@ -20,3 +25,4 @@ def main(argv):
 if __name__ == "__main__":
     import sys
     main(sys.argv)
+    
