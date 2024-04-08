2c5a7725236f3850cb1e019d0ff630d731437bfa
micfun123 <54761507+micfun123@users.noreply.github.com>
2024-04-08 11:19:55 +0100

    Java stuff

---
+++ fibbing.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class fibbing {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        int z = 0;
+        //ask user for number of terms
+        System.out.println("Enter the number of terms: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++){
+            z = a + b;
+            System.out.println(z);
+            a = b;
+            b = z;
+        }
+    } 
+}
---
+++ helloworld.java
@@ -0,0 +1,9 @@
+public class helloworld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+    }
+
+}
