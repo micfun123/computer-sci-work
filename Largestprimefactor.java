8ae917aa1ccd943b93534bf8752888af80b72a7a
micfun123 <54761507+micfun123@users.noreply.github.com>
2024-04-22 13:16:33 +0100

    command line inputs

--- Largestprimefactor.java
+++ Largestprimefactor.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
+
 public class Largestprimefactor {
     public static void main(String[] args) {
-        long num = 600851475143l;
+        long num;
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter a number: ");
+            num = scanner.nextLong();
+        }
         long largest = 0;
         for (long i = 2; i <= num; i++) {
             if (num % i == 0) {
