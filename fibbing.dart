5a5b64c1ee8ed7a654236dcb21a87e74192bea65
micfun123 <54761507+micfun123@users.noreply.github.com>
2024-06-08 22:10:08 +0100

    comments

--- fibbing.dart
+++ fibbing.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+import 'dart:io'; // used to take a input from terminal
 
 
 void main(){
@@ -6,7 +6,7 @@ void main(){
   var b = 1;
   var z = 0;
   print("Enter the amount of of itterations to do: ");
-  int? amount = int.parse(stdin.readLineSync()!);
+  int? amount = int.parse(stdin.readLineSync()!); //takes inpute (for some reason only as a string) then parses it to a int
   for (var i = 0; i < amount; i++) {
     z = a + b;
     print(z);
