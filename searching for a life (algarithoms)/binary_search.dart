ac24e08cf8f40f7d3b9d5c1f8168b135e148e6b6
micfun123 <54761507+micfun123@users.noreply.github.com>
2024-09-11 21:12:05 +0100

    binary_search.dart

---
+++ searching for a life (algarithoms)/binary_search.dart
@@ -0,0 +1,27 @@
+var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 55];
+
+int binary_search(arr,key){
+  var start = 0;
+  var end = arr.length -1;
+  while (start <= end) {
+    var mid = ((start + end) / 2).floor();
+    if (arr[mid] == key) {
+      return mid;
+    }
+    if(arr[mid] > key){
+      end = mid -1;
+    }
+    if(arr[mid] < key){
+      start = mid +1;
+    }
+  }
+  return -1;
+}
+
+void main(){
+  print(binary_search(data, 7));
+  print(binary_search(data, 70));
+  print(binary_search(data, 9));
+  print(binary_search(data, 99));
+  print(binary_search(data, 2));
+}
